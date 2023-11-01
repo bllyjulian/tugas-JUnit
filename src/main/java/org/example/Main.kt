fb6a804e70957b3c2815d30e305be2c953ba6af2
bllyjulian <billyjulian75@gmail.com>
2023-11-01 10:00:55 +0700

    Upload Tugas JUnit

---
+++ src/main/java/org/example/Main.kt
@@ -0,0 +1,12 @@
+package org.example
+
+class Calculator {
+
+    fun add(first: Int, second: Int): Int {
+        return first + second
+    }
+
+    fun sub(first: Int, second: Int): Int {
+        return first - second
+    }
+}
---
+++ src/main/java/org/example/ecommerce/Pesan.kt
@@ -0,0 +1,19 @@
+package org.example.ecommerce
+
+
+class Order(private val product: String, private val quantity: Int) {
+
+    private lateinit var warehouse: Warehouse
+
+    private var isFilled: Boolean = false
+
+
+    fun fill(warehouse: Warehouse) {
+        this.warehouse = warehouse
+        isFilled = this.warehouse.remove(product, quantity)
+    }
+
+    fun isFilled(): Boolean {
+        return isFilled
+    }
+}
